2eeb2cd7722e875a6eafedcedfb26dab434ed8b5
Sandra Venu <sandravenu825@gmail.com>
2026-07-27 04:25:45 +0530

    Configure PostgreSQL

--- hospital/settings.py
+++ hospital/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+    BASE_DIR / "static",
+] if (BASE_DIR / "static").exists() else []
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
